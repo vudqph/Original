0aea82eb00662f2ff2dbdfe94b84d3b1a9c8d43b
Your Name <you@example.com>
2020-12-17 14:19:23 +0700

    update 6/12

---
+++ app/src/main/java/vn/poly/sotaythucung/petsnews/NewsFavoriteActivity.java
@@ -0,0 +1,16 @@
+package vn.poly.sotaythucung.petsnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vn.poly.sotaythucung.R;
+
+public class NewsFavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_favorite);
+    }
+}
